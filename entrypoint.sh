5ac63c57cd45cab224be5140ffc57936a1f1f285
balint-bg <balint@berkshiregrey.com>
2021-12-14 16:51:51 -0500

    Fix parens and add more logging.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,28 +7,30 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-echo "Running: "
-echo  "flakehell lint \
-  | reviewdog -efm="%f:%l:%c: %m" \
-      -name="flakehell" \
-      -reporter="${INPUT_REPORTER:-github-pr-check}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-      -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
-"
-
-flakehell lint > flakehell.out
+export TMPFILE="flakehell.out"
+flakehell lint > ${TMPFILE}
 
 echo "Contents of flakehell.out:"
 echo "**********************"
 cat flakehell.out
 echo "**********************"
 
-cat flakehell.out | reviewdog -efm="%f:%l:%c: %m" \
+
+echo "Running flakehell with:"
+echo "**********************"
+echo 'reviewdog -efm="%f:%l:%c: %m" \
       -name="flakehell" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
       -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
+      ${INPUT_REVIEWDOG_FLAGS} < ${TMPFILE}'
+echo "**********************"
+
+reviewdog -efm="%f:%l:%c: %m" \
+      -name="flakehell" \
+      -reporter="${INPUT_REPORTER:-github-pr-check}" \
+      -filter-mode="${INPUT_FILTER_MODE}" \
+      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+      -level="${INPUT_LEVEL}" \
+      ${INPUT_REVIEWDOG_FLAGS} < ${TMPFILE}
