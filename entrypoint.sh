04235554ecdca1b178b4f1c598e4bc0b050759a1
balint-bg <balint@berkshiregrey.com>
2021-12-14 18:49:13 -0500

    Cleanup before publish.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 set -e
 
-if [ -n "${GITHUB_WORKSPACE}" ] ; then
+export DEFAULT_WORKSPACE=/tmp/flakehell
+
+if [ -n "${GITHUB_WORKSPACE}" ]
+then
+  mkdir "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || true
   cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
+else
+  mkdir flakehell
+  cd flakehell
 fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
@@ -30,6 +37,13 @@ echo 'reviewdog -efm="%f:%l:%c: %m" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
       -level="${INPUT_LEVEL}" \
       ${INPUT_REVIEWDOG_FLAGS} < ${TMPFILE}'
+echo
+echo "Where vars are:"
+echo "  INPUT_REPORTER=${INPUT_REPORTER}"
+echo "  INPUT_FILTER_MODE=${INPUT_FILTER_MODE}"
+echo "  INPUT_FAIL_ON_ERROR=${INPUT_FAIL_ON_ERROR}"
+echo "  INPUT_LEVEL=${INPUT_LEVEL}"
+echo "  INPUT_REVIEWDOG_FLAGS=${INPUT_REVIEWDOG_FLAGS}"
 echo "**********************"
 
 reviewdog -efm="%f:%l:%c: %m" \
