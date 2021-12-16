5dfc7704a28b5d9eb67b7805a870d18fc0055cde
balint-bg <balint@berkshiregrey.com>
2021-12-16 14:27:59 -0500

    Log prettying.

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,23 +11,30 @@ echo "Running in: ${PWD}"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 export TMPFILE=$(mktemp)
 
+echo "============================="
 echo "Flakehell installed plugins:"
 flakehell plugins
 
+echo "============================="
 echo "Flakehell config file:"
 ls -la pyproject.toml
 cat pyproject.toml || true
 
+echo "============================="
 echo "Sanity check, running flakehell without saving its ouput:"
 flakehell lint
 
+echo "============================="
 echo "Running flakehell, saving output to tmpfile: ${TMPFILE}"
 flakehell lint > "${TMPFILE}"
 
+echo "============================="
 ls -la ${TMPFILE}
+echo "============================="
 echo "Head of ${TMPFILE}:"
 head ${TMPFILE}
 
+echo "============================="
 echo "Running reviewdog with:"
 echo 'reviewdog -efm="%f:%l:%c: %m" \
       -name="flakehell" \
@@ -45,6 +52,7 @@ echo "  INPUT_LEVEL=${INPUT_LEVEL}"
 echo "  INPUT_REVIEWDOG_FLAGS=${INPUT_REVIEWDOG_FLAGS}"
 echo
 
+echo "============================="
 reviewdog -efm="%f:%l:%c: %m" \
       -name="flakehell" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
@@ -52,3 +60,6 @@ reviewdog -efm="%f:%l:%c: %m" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
       -level="${INPUT_LEVEL}" \
       ${INPUT_REVIEWDOG_FLAGS} < "${TMPFILE}"
+
+echo "============================="
+echo "Done running reviewdog, exiting..."
